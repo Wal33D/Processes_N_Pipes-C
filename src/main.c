f4280869e1d143bad6fcfc476f28c33f11773b63
Waleed Gudah <aquataze@yahoo.com>
2024-03-08 01:36:27 -0600

    Update build process and main source file for performance improvements

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "utilities.h"
 
-int main() {
+int main()
+{
     int choice;
     char message[1024]; // Used for string operations and reading numbers as strings
 
@@ -17,30 +18,39 @@ int main() {
     pid_t pid;
 
     // Initialize pipes and fork process
-    for (int i = 0; i < PIPE_PAIRS; ++i) {
-        if (pipe(fd + (i * 2)) < 0) {
+    for (int i = 0; i < PIPE_PAIRS; ++i)
+    {
+        if (pipe(fd + (i * 2)) < 0)
+        {
             perror("Pipe initialization failed");
             exit(EXIT_FAILURE);
         }
     }
 
     pid = fork();
-    if (pid < 0) {
+    if (pid < 0)
+    {
         perror("Failed to fork process");
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) { // Child process
+    if (pid == 0)
+    { // Child process
         childProcess(fd, choice);
-    } else { // Parent process
-        if (choice == 4) {
+    }
+    else
+    { // Parent process
+        if (choice == 4)
+        {
             int number;
-            printf(COLOR_GREEN "Parent asks gently: " COLOR_RESET "What number should we play with?\n");
+            printf(COLOR_GREEN "Parent asks: " COLOR_RESET "Enter a number: ");
             scanf("%d", &number);
-            // Convert choice to indicate a math operation specifically, if necessary
-            parentProcess(fd, (char *)&number, choice); // Casting number as char* for compatibility
-        } else {
-            printf(COLOR_GREEN "Parent inquires: " COLOR_RESET "What message shall we transform?\n");
+            // Casting number as char* for compatibility
+            parentProcess(fd, (char *)&number, choice);
+        }
+        else
+        {
+            printf(COLOR_GREEN "Parent asks: " COLOR_RESET "Enter your message: ");
             fgets(message, sizeof(message), stdin);
             message[strcspn(message, "\n")] = 0; // Remove the newline character at the end
             parentProcess(fd, message, choice);
