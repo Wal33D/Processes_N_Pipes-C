03f0bd0583dd9e5912bfdaf45503e4f3a9266025
Waleed Gudah <aquataze@yahoo.com>
2024-03-08 01:14:53 -0600

    Refactor process and pipe handling, update child process management logic

--- src/child.c
+++ src/child.c
@@ -12,30 +12,36 @@ void childProcess(int fd[], int choice) {
     char buffer[1024];
     int length = read(fd[CHILD_READ], buffer, sizeof(buffer) - 1);
     if (length < 0) {
-        perror("Child read failed");
+        perror("Little one stumbled: ");
         exit(EXIT_FAILURE);
     }
     buffer[length] = '\0'; // Null-terminate the string
 
     char *modifiedMessage;
-    // Decide which operation to perform based on the opCode
+    // Enthusiastically accepting the task
     switch(opCode) {
         case 1: // Toggle case
+            printf(COLOR_CYAN "Child giggles: " COLOR_RESET "Oh, I love playing with case! Watch this!\n");
             modifiedMessage = toggleString(buffer);
             break;
         case 2: // Uppercase
+            printf(COLOR_CYAN "Child shouts: " COLOR_RESET "I'll shout this back, louder and prouder!\n");
             modifiedMessage = uppercaseOperation(buffer);
             break;
         case 3: // Create Palindrome
+            printf(COLOR_CYAN "Child muses: " COLOR_RESET "Mirroring is fun, let's make a palindrome!\n");
             modifiedMessage = createPalindrome(buffer);
             break;
         default:
+            printf(COLOR_CYAN "Child is puzzled but obliges: " COLOR_RESET "I'm not quite sure, but here you go!\n");
             modifiedMessage = strdup(buffer); // Default action: echo the message
             break;
     }
 
-    // Send the modified message back to the parent
+    // Cheerfully sending the modified message back
     write(fd[CHILD_WRITE], modifiedMessage, strlen(modifiedMessage) + 1);
+    printf(COLOR_CYAN "Child beams: " COLOR_RESET "Done! Sending it back now!\n");
+    
     free(modifiedMessage); // Free the dynamically allocated memory
 
     // Close the pipes and exit
