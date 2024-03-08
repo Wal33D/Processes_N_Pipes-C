aace0c15a970258ec0a2d64a507c5ef149999794
Waleed Gudah <aquataze@yahoo.com>
2024-03-07 23:05:05 -0600

    Modified utilities module for optimized performance

--- src/utilities.c
+++ src/utilities.c
@@ -5,6 +5,12 @@
 #include <ctype.h>
 #include "utilities.h"
 
+/**
+ * Toggles the case of each character in the input string.
+ * 
+ * @param input The input string whose case is to be toggled.
+ * @return A new string with each character's case toggled.
+ */
 char *toggleString(const char *input) {
     // Allocate memory for the toggled string including the null terminator
     char *toggledStr = malloc(strlen(input) + 1);
@@ -21,7 +27,14 @@ char *toggleString(const char *input) {
     return toggledStr;
 }
 
-
+/**
+ * Validates the command line input to ensure there is exactly one argument provided
+ * and that it contains at least one alphabetical character.
+ * 
+ * @param argc The argument count.
+ * @param argv The argument vector.
+ * @return 0 if the input is valid, EXIT_FAILURE otherwise.
+ */
 int inputValidation(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <'Your string here'>\n", argv[0]);
@@ -40,3 +53,4 @@ int inputValidation(int argc, char *argv[]) {
     printf("Please run the program again with at least one alphabetical character.\n");
     return EXIT_FAILURE;
 }
+
