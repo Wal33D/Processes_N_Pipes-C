bd3b0785bc1997e2c7cfcf63246388e70d9650d6
Waleed Gudah <aquataze@yahoo.com>
2024-03-08 01:01:44 -0600

    Refactor and improve process handling in `processes-n-pipes`, update utility functions in `utilities.h`, enhance parent process logic in `src/parent.c`

--- include/utilities.h
+++ include/utilities.h
@@ -3,33 +3,35 @@
 
 // Standard library includes
 #include <assert.h>
-#include <ctype.h>      // For isalpha, tolower, toupper
-#include <stdbool.h>    // For bool type
-#include <stdio.h>      // For fprintf, printf, perror
-#include <stdlib.h>     // For malloc, free, exit
-#include <string.h>     // For strlen, strcpy
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 // Definitions for pipe indices and the number of pipe pairs
-#define CHILD_READ   2
-#define CHILD_WRITE  1
-#define PARENT_READ  0
+#define CHILD_READ 2
+#define CHILD_WRITE 1
+#define PARENT_READ 0
 #define PARENT_WRITE 3
-#define PIPE_PAIRS   2
+#define PIPE_PAIRS 2
 
 // ANSI Color Code Definitions
-#define COLOR_CYAN   "\x1b[36m"
-#define COLOR_GREEN  "\x1b[32m"
-#define COLOR_RESET  "\x1b[0m"
-#define COLOR_WHITE  "\x1b[37m"
+#define COLOR_CYAN "\x1b[36m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_RESET "\x1b[0m"
+#define COLOR_WHITE "\x1b[37m"
 #define COLOR_YELLOW "\x1b[33m"
 
 // Function declarations
 char *toggleString(const char *input);
 int inputValidation(int argc, char *argv[]);
-void childProcess(int fd[]);
-void parentProcess(int fd[], char *message);
+
+void childProcess(int fd[], int choice);
+void parentProcess(int fd[], char *message, int choice);
+
 char *createPalindrome(const char *word);
 char *uppercaseOperation(const char *input);
 
--- src/parent.c
+++ src/parent.c
@@ -5,21 +5,28 @@ void parentProcess(int fd[], char *message, int choice) {
     close(fd[CHILD_READ]);
     close(fd[CHILD_WRITE]);
 
+    printf(COLOR_YELLOW "Parent Process: Sending '" COLOR_WHITE "%s" COLOR_YELLOW "' to Child" COLOR_RESET "\n", message);
     // First, send the operation code to the child
-    write(fd[PARENT_WRITE], &choice, sizeof(choice));
+    if (write(fd[PARENT_WRITE], &choice, sizeof(choice)) != sizeof(choice)) {
+        fprintf(stderr, COLOR_WHITE "Parent write (operation code) failed" COLOR_RESET "\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Then, send the message
-    write(fd[PARENT_WRITE], message, strlen(message) + 1);
+    if (write(fd[PARENT_WRITE], message, strlen(message) + 1) != strlen(message) + 1) {
+        fprintf(stderr, COLOR_WHITE "Parent write (message) failed" COLOR_RESET "\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Wait for the modified message from the child
     int length = read(fd[PARENT_READ], buffer, sizeof(buffer));
     if (length < 0) {
-        perror("Parent read failed");
+        fprintf(stderr, COLOR_WHITE "Parent read failed" COLOR_RESET "\n");
         exit(EXIT_FAILURE);
     }
     buffer[length] = '\0';
 
-    printf("Parent Process: Received '%s'\n", buffer);
+    printf(COLOR_YELLOW "Parent Process: Received '" COLOR_WHITE "%s" COLOR_YELLOW "' from Child" COLOR_RESET "\n", buffer);
 
     close(fd[PARENT_READ]);
     close(fd[PARENT_WRITE]);
