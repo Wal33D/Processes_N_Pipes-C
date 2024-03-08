1dea61e4bd53ec3672995461cdd0a59b87383df1
Waleed Gudah <aquataze@yahoo.com>
2024-03-08 03:04:39 -0600

    Updated build scripts and modified child and parent components for enhanced process management

--- src/child.c
+++ src/child.c
@@ -1,111 +1,81 @@
 #include "utilities.h"
 
-void childProcess(int fd[], int choice)
-{
-    // Close the pipes not used by the child process
+void childProcess(int fd[], int choice) {
+    // Close unused ends of the pipes
     close(fd[PARENT_READ]);
     close(fd[PARENT_WRITE]);
-
-    // Check if the operation is a math operation
-    if (choice == 4)
-    {
+  if (choice == 4) {
+        // Handling a random math operation
         int number;
-        // Read the number for the operation from the parent
         read(fd[CHILD_READ], &number, sizeof(number));
-        // Initialize random number generator
-        srand(time(NULL));
+        srand(time(NULL)); // Seed the random number generator
+        
+        int operationNumber = rand() % 10; // Generate a number for the operation, between 0-9
+        int originalNumber = number; // Keep the original number for printing
 
-        int operationNumber = rand() % 10;
-        int originalNumber = number;
-
-        // Perform a random math operation on the number
-        switch (rand() % 3)
-        {
-        case 0:
-            number += operationNumber;
-            printf(COLOR_CYAN "Child calculates: " COLOR_RESET "Adding %d to %d, we get %d!\n", operationNumber, originalNumber, number);
-            break;
-        case 1:
-            number -= operationNumber;
-            printf(COLOR_CYAN "Child ponders: " COLOR_RESET "Taking %d away from %d, it becomes %d!\n", operationNumber, originalNumber, number);
-            break;
-        case 2:
-            operationNumber = (rand() % 3) + 1;
-            number *= operationNumber;
-            printf(COLOR_CYAN "Child dreams: " COLOR_RESET "Multiplying %d by %d... Look! It's now %d!\n", originalNumber, operationNumber, number);
-            break;
+        switch (rand() % 3) { // Choose a random operation: 0=add, 1=subtract, 2=multiply
+            case 0:
+                number += operationNumber; // Add the operation number
+                printf(COLOR_CYAN "Child calculates: " COLOR_RESET "Adding %d to %d, we get %d!\n", operationNumber, originalNumber, number);
+                break;
+            case 1:
+                number -= operationNumber; // Subtract the operation number
+                printf(COLOR_CYAN "Child ponders: " COLOR_RESET "Taking %d away from %d, it becomes %d!\n", operationNumber, originalNumber, number);
+                break;
+            case 2:
+                // For multiplication, let's adjust the range to 1-3 for a clearer result
+                operationNumber = (rand() % 3) + 1; // Adjusting range for multiplication
+                number *= operationNumber; // Multiply by the operation number
+                printf(COLOR_CYAN "Child dreams: " COLOR_RESET "Multiplying %d by %d... Look! It's now %d!\n", originalNumber, operationNumber, number);
+                break;
         }
 
         // Send the result back to the parent
         write(fd[CHILD_WRITE], &number, sizeof(number));
         printf(COLOR_CYAN "Child smiles: " COLOR_RESET "All done with the numbers! Sending back a surprise!\n");
-    }
-    else
-    {
-        // Handle string operations
+
+    } else {
+        // First, read the operation code
         int opCode;
-        // Read the operation code from the parent
         read(fd[CHILD_READ], &opCode, sizeof(opCode));
 
         char buffer[1024];
-        // Read the string for the operation
         int length = read(fd[CHILD_READ], buffer, sizeof(buffer) - 1);
-        if (length < 0)
-        {
+        if (length < 0) {
             perror("Little one stumbled: ");
             exit(EXIT_FAILURE);
         }
-        buffer[length] = '\0';
+        buffer[length] = '\0'; // Null-terminate the string
 
-        // Process the string based on the operation code
         char *modifiedMessage;
-        switch (opCode)
-        {
-        case 1:
-            modifiedMessage = toggleString(buffer);
-            printf(COLOR_CYAN "Child giggles: " COLOR_RESET "Oh, I love playing with case! Watch this!\n");
-            break;
-        case 2:
-            modifiedMessage = uppercaseOperation(buffer);
-            printf(COLOR_CYAN "Child shouts: " COLOR_RESET "I'll shout this back, louder and prouder!\n");
-            break;
-        case 3:
-            modifiedMessage = createPalindrome(buffer);
-            printf(COLOR_CYAN "Child muses: " COLOR_RESET "Mirroring is fun, let's make a palindrome!\n");
-            break;
-        default:
-            modifiedMessage = strdup(buffer);
-            printf(COLOR_CYAN "Child is puzzled but obliges: " COLOR_RESET "I'm not quite sure, but here you go!\n");
-            break;
+        // Enthusiastically accepting the task
+        switch(opCode) {
+            case 1: // Toggle case
+                printf(COLOR_CYAN "Child giggles: " COLOR_RESET "Oh, I love playing with case! Watch this!\n");
+                modifiedMessage = toggleString(buffer);
+                break;
+            case 2: // Uppercase
+                printf(COLOR_CYAN "Child shouts: " COLOR_RESET "I'll shout this back, louder and prouder!\n");
+                modifiedMessage = uppercaseOperation(buffer);
+                break;
+            case 3: // Create Palindrome
+                printf(COLOR_CYAN "Child muses: " COLOR_RESET "Mirroring is fun, let's make a palindrome!\n");
+                modifiedMessage = createPalindrome(buffer);
+                break;
+            default:
+                printf(COLOR_CYAN "Child is puzzled but obliges: " COLOR_RESET "I'm not quite sure, but here you go!\n");
+                modifiedMessage = strdup(buffer); // Default action: echo the message
+                break;
         }
 
-        // Send the modified message back to the parent
+        // Cheerfully sending the modified message back
         write(fd[CHILD_WRITE], modifiedMessage, strlen(modifiedMessage) + 1);
         printf(COLOR_CYAN "Child beams: " COLOR_RESET "Done! Sending it back now!\n");
 
-        // Free the dynamically allocated memory
-        free(modifiedMessage);
+        free(modifiedMessage); // Free the dynamically allocated memory
     }
 
-    // Wait for a "Thank You" message from the parent
-    char thankYouMsg[1024];
-    int msgLen = read(fd[CHILD_READ], thankYouMsg, sizeof(thankYouMsg) - 1);
-    if (msgLen > 0)
-    {
-        thankYouMsg[msgLen] = '\0';
-        // Print the "Thank You" message
-        printf(COLOR_CYAN "Child hears: " COLOR_RESET "%s\n", thankYouMsg);
-
-        // Send a response back to the parent
-        char responseToParent[] = "No problem, see ya later!";
-        write(fd[CHILD_WRITE], responseToParent, sizeof(responseToParent));
-    }
-    else
-    {
-        fprintf(stderr, "Error reading 'Thank You' message from parent.\n");
-    }
-
-    // Close the pipes and exit the child process
+    // Close the pipes and exit
     close(fd[CHILD_READ]);
     close(fd[CHILD_WRITE]);
     exit(EXIT_SUCCESS);
--- src/parent.c
+++ src/parent.c
@@ -2,47 +2,44 @@
 
 void parentProcess(int fd[], char *message, int choice)
 {
-    // Close the pipe ends that are not used by the parent
+    // Close unused ends of the pipes
     close(fd[CHILD_READ]);
     close(fd[CHILD_WRITE]);
 
-    // Send different messages based on the choice of operation
+    // A playful message for sending the operation to the child
     switch (choice)
     {
     case 1:
-        // Request to toggle the case of the message
         printf(COLOR_GREEN "Parent whispers: " COLOR_RESET "Hey sweetie, can you " COLOR_YELLOW "toggle" COLOR_RESET " this for me? --> '" COLOR_WHITE "%s" COLOR_RESET "'\n", message);
         break;
     case 2:
-        // Request to convert the message to uppercase
         printf(COLOR_GREEN "Parent muses: " COLOR_RESET "Darling, could you please " COLOR_YELLOW "shout" COLOR_RESET " this out loud? --> '" COLOR_WHITE "%s" COLOR_RESET "'\n", message);
         break;
     case 3:
-        // Request to convert the message to a palindrome
         printf(COLOR_GREEN "Parent ponders: " COLOR_RESET "Oh, what if we made this " COLOR_YELLOW "mirror" COLOR_RESET " itself? --> '" COLOR_WHITE "%s" COLOR_RESET "'\n", message);
         break;
     case 4:
-        // Request a random math operation on a number
+        // Correctly interpret the message as a pointer to an integer
         int *pNumber = (int *)message;
         printf(COLOR_GREEN "Parent quizzes: " COLOR_RESET "I wonder how we can play with the number " COLOR_YELLOW "%d" COLOR_RESET " today?\n", *pNumber);
+        // Then send the number to the child
         write(fd[PARENT_WRITE], pNumber, sizeof(int));
         break;
     default:
-        // Fallback message for unrecognized choice
         printf(COLOR_GREEN "Parent is confused: " COLOR_RESET "Hmm, not sure what you want, but let's try '" COLOR_WHITE "%s" COLOR_RESET "' anyway.\n", message);
         break;
     }
 
-    // Send the operation code to the child process
+    // Send the operation code to the child
     if (write(fd[PARENT_WRITE], &choice, sizeof(choice)) != sizeof(choice))
     {
         fprintf(stderr, COLOR_WHITE "Parent sighs: " COLOR_RESET "Oh dear, I couldn't get your sibling to listen...\n");
         exit(EXIT_FAILURE);
     }
 
-    // If not performing a math operation, send the message to the child
     if (choice != 4)
-    {
+ {
+        // Send the message for string operations
         if (write(fd[PARENT_WRITE], message, strlen(message) + 1) != strlen(message) + 1)
         {
             fprintf(stderr, COLOR_WHITE "Parent sighs: " COLOR_RESET "Oopsie daisy, my message got lost in the mail...\n");
@@ -50,26 +47,22 @@ void parentProcess(int fd[], char *message, int choice)
         }
     }
 
-    // Buffer to store the response from the child process
-    char buffer[1024];
-    int length;
-
-    // Process the response from the child, either a modified message or a math result
+    // Wait for and display the modified message or result from the child
     if (choice == 4)
     {
         int result;
-        if ((length = read(fd[PARENT_READ], &result, sizeof(result))) < 0)
+        if (read(fd[PARENT_READ], &result, sizeof(result)) < 0)
         {
             fprintf(stderr, COLOR_WHITE "Parent frets: " COLOR_RESET "Gosh, I never heard back. Are they ignoring me?\n");
             exit(EXIT_FAILURE);
         }
-        // Display the result of the math operation
         printf(COLOR_GREEN "Parent beams proudly: " COLOR_RESET "Look! The number magic turned it into " COLOR_YELLOW "%d" COLOR_RESET "!\n", result);
     }
     else
     {
-        // Display the modified message
-        if ((length = read(fd[PARENT_READ], buffer, sizeof(buffer) - 1)) < 0)
+        char buffer[1024];
+        int length = read(fd[PARENT_READ], buffer, sizeof(buffer));
+        if (length < 0)
         {
             fprintf(stderr, COLOR_WHITE "Parent frets: " COLOR_RESET "Gosh, I never heard back. Are they ignoring me?\n");
             exit(EXIT_FAILURE);
@@ -78,21 +71,7 @@ void parentProcess(int fd[], char *message, int choice)
         printf(COLOR_GREEN "Parent beams proudly: " COLOR_RESET "Oh, look at what my clever child did! --> '" COLOR_YELLOW "%s" COLOR_RESET "'\n", buffer);
     }
 
-    // Send a thank you message to the child for their work
-    char thankYouMsg[] = "Thanks for talking, kiddo!";
-    write(fd[PARENT_WRITE], thankYouMsg, sizeof(thankYouMsg));
-
-    // Wait for the child's final response to the thank you message
-    if ((length = read(fd[PARENT_READ], buffer, sizeof(buffer) - 1)) < 0)
-    {
-        fprintf(stderr, COLOR_WHITE "Parent frets: " COLOR_RESET "Seems I can't hear back from my child.\n");
-        exit(EXIT_FAILURE);
-    }
-    buffer[length] = '\0';
-    // Display the child's response to the thank you message
-    printf(COLOR_GREEN "Parent smiles warmly: " COLOR_RESET "%s\n", buffer);
-
     close(fd[PARENT_READ]);
     close(fd[PARENT_WRITE]);
-    wait(NULL);
+    wait(NULL); // Wait for the child process to terminate
 }
